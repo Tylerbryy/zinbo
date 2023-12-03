aab0a5266fd990a1b037a4bbc87699f4200ceb23
tylerbry <tylergibbs049@gmail.com>
2023-12-02 18:54:35 -0600

    added email recovery

--- src/email_processing.py
+++ src/email_processing.py
@@ -4,37 +4,50 @@ from openai import OpenAI
 
 from colorama import Fore
 from src.email_evaluation import evaluate_email
+import json
 
 
 
-def process_email(gmail: Resource, message_info: Dict[str, Union[str, List[str]]], email_data_parsed: Dict[str, Union[str, List[str]]], user_first_name: str, user_last_name: str, client: OpenAI, action: str) -> int:
+processed_emails_details = []  # Global list to store processed email details
+
+
+def process_email(gmail: Resource, message_info: Dict[str, Union[str, List[str]]], email_data_parsed: Dict[str, Union[str, List[str]]], user_first_name: str, user_last_name: str, client: OpenAI, action: str, processed_emails_file_path: str) -> int:
     # Evaluate email
     if evaluate_email(email_data_parsed, user_first_name, user_last_name, client):
+        
+        # Prepare email details for tracking
+        email_details = {
+            'id': message_info['id'],
+            'subject': email_data_parsed.get('subject', ''),
+            'from': email_data_parsed.get('from', ''),
+            'email_contents': email_data_parsed['body'],
+            'action': action
+        }
+        
         if action == 'delete':
             print(Fore.LIGHTYELLOW_EX + "Email is not worth the time, deleting" + Fore.RESET)
             # Delete email
             try:
-                gmail.users().messages().delete(
-                    userId='me',
-                    id=message_info['id']
-                ).execute()
+                gmail.users().messages().delete(userId='me', id=message_info['id']).execute()
                 print(Fore.LIGHTGREEN_EX + "Email deleted successfully" + Fore.RESET)
-                return 1
             except Exception as e:
                 print(Fore.LIGHTRED_EX + f"Failed to delete email: {e}" + Fore.RESET)
+                return 0
         elif action == 'read':
             print(Fore.LIGHTYELLOW_EX + "Email is not worth the time, marking as read" + Fore.RESET)
             # Remove UNREAD label
             try:
-                gmail.users().messages().modify(
-                    userId='me',
-                    id=message_info['id'],
-                    body={'removeLabelIds': ['UNREAD']}
-                ).execute()
+                gmail.users().messages().modify(userId='me', id=message_info['id'], body={'removeLabelIds': ['UNREAD']}).execute()
                 print(Fore.LIGHTGREEN_EX + "Email marked as read successfully" + Fore.RESET)
-                return 1
             except Exception as e:
                 print(Fore.LIGHTRED_EX + f"Failed to mark email as read: {e}" + Fore.RESET)
+                return 0
+        # Append to the global list
+        processed_emails_details.append(email_details)
+        # Write to the file after each action
+        with open(processed_emails_file_path, 'w') as file:
+            json.dump(processed_emails_details, file, indent=4)
+        return 1
     else:
         print(Fore.LIGHTBLUE_EX + "Email is worth the time, leaving as unread" + Fore.RESET)
     return 0
