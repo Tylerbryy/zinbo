3d92161bf37a3c323753876ddb2feb7460461651
tylerbry <tylergibbs049@gmail.com>
2023-12-06 12:13:00 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,79 +1,73 @@
-import subprocess
 import os
+import subprocess
+from huggingface_hub import hf_hub_download
 
 
-# Define the path to your .env file
-env_file_path = '.env'
-models_dir = 'models'  # Define the models directory
-
-# Ensure the models directory exists
-os.makedirs(models_dir, exist_ok=True)
-
-# Detect the operating system and set the environment variable for symlinks if on Windows
-if os.name == 'nt':   
-    os.environ['HF_HUB_DISABLE_SYMLINKS_WARNING'] = '1'
+# Define paths
+env_file_path = ".env"
+models_dir = "models"
 
 
-# Function to install dependencies from requirements.txt
+def ensure_models_dir():
+    os.makedirs(models_dir, exist_ok=True)
+
+
+# Detect and handle OS-specific environment variables
+def detect_os():
+    os_name = os.name
+    if os_name == "nt":
+        os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+    return "Mac" if os_name == "posix" else "Windows"
+
+
+# Install dependencies
 def install_requirements():
     subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
-install_requirements()
 
 
-from huggingface_hub import hf_hub_download
-# Function to update the .env file with the new model path
-def update_env_file(key, new_value):
-    # Check if the .env file exists
+# Update .env file
+def update_env(key, new_value):
+    # Read and update existing content
     if os.path.exists(env_file_path):
-        # Read the current contents of the file
-        with open(env_file_path, 'r') as file:
-            lines = file.readlines()
+        with open(env_file_path, "r") as f:
+            lines = f.readlines()
 
-        # Update the specified key with the new value
-        with open(env_file_path, 'w') as file:
+        with open(env_file_path, "w") as f:
             found = False
             for line in lines:
-                if line.startswith(key):
-                    file.write(f'{key}="{new_value}"\n')
+                if line.startswith(f"{key}="):
+                    f.write(f"{key}={new_value}\n")
                     found = True
                 else:
-                    file.write(line)
-            if not found:
-                file.write(f'{key}="{new_value}"\n')
+                    f.write(line)
+        if not found:
+            f.write(f"{key}={new_value}\n")
+    # Create new .env file
     else:
-        # Create the .env file if it does not exist
-        with open(env_file_path, 'w') as file:
-            file.write(f'{key}="{new_value}"\n')
-
-def update_operating_system_env():
-    # Detect the operating system
-    operating_system = 'Mac' if os.name == 'posix' else 'Windows'
-    
-    # Update the .env file with the detected operating system
-    update_env_file('OPERATING_SYSTEM', operating_system)
-
-# Call the function to update the OPERATING_SYSTEM environment variable
-update_operating_system_env()
+        with open(env_file_path, "w") as f:
+            f.write(f"{key}={new_value}\n")
 
 
-# Function to download models from Hugging Face and update .env
+# Download and update models
 def download_models():
-    # Define the model repository IDs and filenames
+    # Model data
     models = {
-        'TheBloke/OpenHermes-2.5-Mistral-7B-GGUF': ('openhermes-2.5-mistral-7b.Q4_K_M.gguf', 'LOCAL_OPEN_HERMES_LOCATION'),
-        'TheBloke/Llama-2-7B-Chat-GGUF': ('llama-2-7b-chat.Q4_K_M.gguf', 'LOCAL_LLAMA_LOCATION'),
-        # Add more models here if necessary
+        "TheBloke/OpenHermes-2.5-Mistral-7B-GGUF": ("openhermes-2.5-mistral-7b.Q4_K_M.gguf", "LOCAL_OPEN_HERMES_LOCATION"),
+        "TheBloke/Llama-2-7B-Chat-GGUF": ("llama-2-7b-chat.Q4_K_M.gguf", "LOCAL_LLAMA_LOCATION"),
     }
 
-    # Download each model and update the .env file
+    # Download and update
     for repo_id, (filename, env_key) in models.items():
         print(f"Downloading {filename} from {repo_id}...")
-        
+
         model_path = hf_hub_download(repo_id=repo_id, filename=filename, cache_dir=models_dir)
-        
-        # Update the .env file with the new model path
-        update_env_file(env_key, model_path)
 
-# Post-installation: Install requirements and download models
+        update_env(env_key, model_path)
 
+
+# Main execution
+ensure_models_dir()
+operating_system = detect_os()
+install_requirements()
+update_env("OPERATING_SYSTEM", operating_system)
 download_models()
